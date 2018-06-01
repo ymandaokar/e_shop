90fe52e1d5fda74ec90c4f70b6bea439b625a3bf
PARACORP\ymandaokar <ymandaokar@paragyte.com>
2018-06-02 01:58:46 +0530

    categories done.

--- packages/ui/src/js/components/categories.js
+++ packages/ui/src/js/components/categories.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
-
+import classNames from "classnames";
+import { Manager, Target, Popper } from "react-popper";
+import { withStyles } from "material-ui/styles";
+import Button from "material-ui/Button";
+import ClickAwayListener from "material-ui/utils/ClickAwayListener";
+import Collapse from "material-ui/transitions/Collapse";
+import Grow from "material-ui/transitions/Grow";
+import Paper from "material-ui/Paper";
+import Portal from "material-ui/Portal";
+import MenuItem from "material-ui/Menu/MenuItem";
+import MenuList from "material-ui/Menu/MenuList";
+import history from "../helpers/history";
+import Down from "material-ui-icons/KeyboardArrowDown";
+import Up from "material-ui-icons/KeyboardArrowUp";
+const styles = {
+  root: {
+    flexGrow: 1,
+    height: "100%"
+  },
+  flex: {
+    flex: 1
+  }
+};
 class categories extends Component {
-  handleCategoryClick(id) {}
+  constructor() {
+    super();
+    this.state = { category: "" };
+    this.handleClose = this.handleClose.bind(this);
+  }
+  handleCategoryClick(id, redirect) {
+    if (redirect) {
+      history.push({ pathname: `/products/search/${id}` });
+    } else {
+      this.setState({ category: id });
+    }
+  }
+  handleClose() {
+    this.setState({ category: "" });
+  }
   render() {
-    let { categories, isMobile } = this.props;
+    let { categories, isMobile, classes } = this.props;
     return (
       <div className="row center-xs" style={{ background: "#fcf9f9b5" }}>
         <div
@@ -24,15 +60,71 @@ class categories extends Component {
                   {categories.map(category => {
                     return (
                       <div className="homeCategory" key={category.id}>
-                        <button
-                          className="btn-link homeCatButton"
-                          onClick={this.handleCategoryClick.bind(
-                            this,
-                            category.id
+                        <Manager>
+                          <Target>
+                            <button
+                              className="btn-link homeCatButton"
+                              style={{ display: "flex" }}
+                              onClick={this.handleCategoryClick.bind(
+                                this,
+                                category.id,
+                                !category.children.length
+                              )}
+                            >
+                              {category.Name}
+                              {!!category.children.length &&
+                                (category.id == this.state.category ? (
+                                  <Up
+                                    style={{ marginTop: -3, marginLeft: 5 }}
+                                  />
+                                ) : (
+                                  <Down
+                                    style={{ marginTop: -3, marginLeft: 5 }}
+                                  />
+                                ))}
+                            </button>
+                          </Target>
+                          {!!category.children.length && (
+                            <Popper
+                              placement="bottom-end"
+                              eventsEnabled={category.id == this.state.category}
+                              className={classNames({
+                                [classes.popperClose]: !(
+                                  category.id == this.state.category
+                                )
+                              })}
+                            >
+                              <ClickAwayListener onClickAway={this.handleClose}>
+                                <Grow
+                                  in={category.id == this.state.category}
+                                  id="menu-list-grow"
+                                  style={{ transformOrigin: "0 0 0" }}
+                                >
+                                  <Paper>
+                                    <MenuList role="menu" style={{ margin: 6 }}>
+                                      {category.children.map(child => {
+                                        return (
+                                          <MenuItem
+                                            key={child.id}
+                                            onClick={this.handleCategoryClick.bind(
+                                              this,
+                                              child.id,
+                                              true
+                                            )}
+                                            className="homeCatButton"
+                                            style={{ fontSize: "80%" }}
+                                          >
+                                            {child.Name}
+                                          </MenuItem>
+                                        );
+                                      })}
+                                    </MenuList>
+                                  </Paper>
+                                </Grow>
+                              </ClickAwayListener>
+                            </Popper>
                           )}
-                        >
-                          {category.Name}
-                        </button>
+                        </Manager>
                       </div>
                     );
                   })}
@@ -45,4 +137,4 @@ class categories extends Component {
   }
 }
 
-export default categories;
+export default withStyles(styles)(categories);
