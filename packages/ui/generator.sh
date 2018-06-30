a6ecb0a7543fceedb3c8fac71cabda8a51b9ee13
PARACORP\ymandaokar <ymandaokar@paragyte.com>
2018-06-30 22:34:45 +0530

    docker update.

--- packages/ui/generator.sh
+++ packages/ui/generator.sh
@@ -2,8 +2,8 @@
 echo "file generating..."
 cat > /etc/nginx/conf.d/nginx.conf << EOF
 
-upstream surveyapi {
-    server $SURVEY_API_SERVICE_HOST:$SURVEY_API_SERVICE_PORT;
+upstream tofaapi {
+    server $TOFA_API_SERVICE_HOST:$TOFA_API_SERVICE_PORT;
 }
 upstream couchapi {
     server  $COUCHDB_SERVICE_HOST:$COUCHDB_SERVICE_PORT;
@@ -12,7 +12,7 @@ server {
     listen       80;
     access_log   /etc/nginx/conf.d/localhost.access.log  main;
 
-    root  /var/www/sites/survey;
+    root  /var/www/sites/tofa;
     
     index index.html;
 
@@ -28,7 +28,7 @@ server {
     }
 
     location /user {
-        proxy_pass http://surveyapi/user;
+        proxy_pass http://tofaapi/user;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -38,7 +38,7 @@ server {
     }
 
     location /auth {
-        proxy_pass http://surveyapi/auth;
+        proxy_pass http://tofaapi/auth;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -47,8 +47,8 @@ server {
         proxy_pass_request_headers on;
     }
 
-    location /trigger {
-        proxy_pass http://surveyapi/trigger;
+    location /categories {
+        proxy_pass http://tofaapi/categories;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -57,8 +57,8 @@ server {
         proxy_pass_request_headers on;
     }
 
-     location /survey {
-        proxy_pass http://surveyapi/survey;
+     location /productitems {
+        proxy_pass http://tofaapi/productitems;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -67,8 +67,8 @@ server {
         proxy_pass_request_headers on;
     }
 
-    location /published {
-        proxy_pass http://surveyapi/published;
+    location /organization {
+        proxy_pass http://tofaapi/organization;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -77,108 +77,8 @@ server {
         proxy_pass_request_headers on;
     }
 
-    location /thumbnail {
-        proxy_pass http://surveyapi/thumbnail;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /response {
-        proxy_pass http://surveyapi/response;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /validateuserlocation {
-        proxy_pass http://surveyapi/validateuserlocation;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /validatesurveytime {
-        proxy_pass http://surveyapi/validatesurveytime;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /providers {
-        proxy_pass http://surveyapi/providers;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /validate {
-        proxy_pass http://surveyapi/validate;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /media {
-        proxy_pass http://surveyapi/media;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /clientimage {
-        proxy_pass http://surveyapi/clientimage;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /sharedContent {
-        proxy_pass http://surveyapi/sharedContent;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /toJSON {
-        proxy_pass http://surveyapi/toJSON;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-For \$remote_addr;
-        proxy_buffering off;
-        proxy_pass_request_headers on;
-    }
-
-    location /invite {
-        proxy_pass http://surveyapi/invite;
+    location /invoice {
+        proxy_pass http://tofaapi/invoice;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -188,7 +88,7 @@ server {
     }
 
     location /healthz {
-        proxy_pass http://surveyapi/healthz;
+        proxy_pass http://tofaapi/healthz;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-Proto \$scheme;
@@ -201,7 +101,7 @@ server {
         return 404;
     }
 
-    location ~ ^/db\/((survey\\$|survey%24)[^/]*)\/(_all_docs|_bulk_docs|_changes|_local|_design|_revs_diff|_bulk_get|_find)\/(.*)$ {
+    location ~ ^/db\/((ecart\\$|ecart%24)[^/]*)\/(_all_docs|_bulk_docs|_changes|_local|_design|_revs_diff|_bulk_get|_find)\/(.*)$ {
         proxy_pass http://couchapi/\$1/\$3/\$4\$is_args\$args;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
@@ -211,7 +111,7 @@ server {
         proxy_pass_request_headers on;
     }
 
-    location ~ ^/db\/((survey\\$|survey%24)[^/]*)\/(\$|_all_docs|_bulk_docs|_changes|_local|_design|_revs_diff|_bulk_get|_find)$ {
+    location ~ ^/db\/((ecart\\$|ecart%24)[^/]*)\/(\$|_all_docs|_bulk_docs|_changes|_local|_design|_revs_diff|_bulk_get|_find)$ {
         proxy_pass http://couchapi/\$1/\$3\$is_args\$args;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
@@ -221,7 +121,7 @@ server {
         proxy_pass_request_headers on;
     }
 
-    location ~ ^/db\/((survey\\$|survey%24)[^/]*)\/([^\_].*)$ {
+    location ~ ^/db\/((ecart\\$|ecart%24)[^/]*)\/([^\_].*)$ {
         proxy_pass http://couchapi/\$1/\$3\$is_args\$args;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
